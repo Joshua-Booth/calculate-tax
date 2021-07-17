99db21442636dbf2e7e88854bfd2447115846267
Joshua Booth <me@joshuabooth.nz>
2021-07-17 20:42:17 +1200

    style: change footer link colour to grey

--- src/components/Layout/Footer/TextLinks.js
+++ src/components/Layout/Footer/TextLinks.js
@@ -15,8 +15,7 @@ import { FOOTER_LINKS } from "constants/links";
  * @returns {React.Component} The text links component
  */
 function TextLinks() {
-  const linkClasses =
-    "footer-link no-underline text-dark-grey hover:text-primary";
+  const linkClasses = "footer-link no-underline text-grey hover:text-primary";
 
   return (
     <div className="flex flex-wrap justify-between bottom-0 w-full mb-5 text-xs">
